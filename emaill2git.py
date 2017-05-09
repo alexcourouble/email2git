190b8198f201b908e596d432bea264635ef4fcb3
Alexandre Courouble <courouble.alexandre@gmail.com>
2017-05-09 14:36:19 -0400

    modified the main script to print found matches and keep non-matches for later use (line matching)

--- emaill2git.py
+++ emaill2git.py
@@ -5,38 +5,46 @@ Author: Alex Courouble <alex.courouble@gmail.com>
 import sqlite3
 
 
-PW_DB = 'pw/pwSubjectFull.db'
-GIT_DB = 'git/commit_subject_full.db'
+PW_DB = '/Users/alexandrecourouble/Desktop/email2git_data/pwSubject.db'
+GIT_DB = '/Users/alexandrecourouble/Desktop/email2git_data/commit_subject.db'
+
+OUTPUT = '/Users/alexandrecourouble/Desktop/email2git_data/SUBJECT_OUTPUT.txt'
+
+def getSubjectMatches():
+	connGIT = sqlite3.connect(GIT_DB) # connecting
+	connGIT.text_factory = str
+	cg = connGIT.cursor() #creating cursor
+
+	gitCommits = {}
+	for row in cg.execute('select * from subject'):
+		gitCommits[row[0]] = (row[1],row[2])
+
+	connGIT.close()
 
 
-connGIT = sqlite3.connect(GIT_DB) # connecting
-connGIT.text_factory = str
-cg = connGIT.cursor() #creating cursor
+	notMatched = [] # list containing all cids that weren't matched
+	connPW = sqlite3.connect(PW_DB) # connecting
+	connPW.text_factory = str
+	cp = connPW.cursor() # creating cursor
 
-gitCommits = {}
-for row in cg.execute('select * from subject'):
-	gitCommits[row[0]] = (row[1],row[2])
+	with open(OUTPUT,"w") as matches: # opening file for writting subject matches
+		for i in gitCommits:
+			string = '%'+gitCommits[i][1]+'%'
+			count = 0
+			for j in cp.execute("""select * from subject where subject like ?;""", (string,)):
+				# print str(j[0])+','+i
+				matches.write(str(j[0])+','+i)
+				count += 1
+			if count == 0: notMatched.append(i)
 
-connGIT.close()
-
-
-notMatched = [] # list containing all cids that weren't matched
-connPW = sqlite3.connect(PW_DB) # connecting
-connPW.text_factory = str
-cp = connPW.cursor() # creating cursor
-for i in gitCommits:
-	string = '%'+gitCommits[i][1]+'%'
-	count = 0
-	for j in cp.execute("""select * from subject where subject like ?;""", (string,)):
-		print str(j[0])+','+i
-		count += 1
-	if count == 0: notMatched.append(i)
-
-# printing remaining cids
+	# printing remaining cids
+	connPW.close()
+		
+	# with open(OUTPUT,"w") as no_match:
+	# 	for i in notMatched:
+	# 		no_match.write(i+"\n")
 
 
-connPW.close()
-	
-with open("remaining_cids_full.txt","w") as no_match:
-	for i in notMatched:
-		no_match.write(i+"\n")
+
+if __name__ == '__main__':
+	getSubjectMatches()
