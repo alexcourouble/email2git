6543f498b588ffabf0a74bd6911f6afba06a52b3
Alexandre Courouble <courouble.alexandre@gmail.com>
2017-05-09 13:19:21 -0400

    New git commit subject generator script

---
+++ subject_data_gen/git/commit_subject_generator.py
@@ -0,0 +1,34 @@
+"""
+This script will create a database containing the first line of the commit message, the commit ID, and the commit time.
+
+How to run:
+enter the follwoing command in the linux git repo. The path should be the path to this script. The date is the lower limit of the git log data
+git log --no-merges --pretty=format:"%H    %ct    %s" --after={2009-01-13} | python /Users/alexandrecourouble/Desktop/workspace_rec_prec/scripts/commit_subject_generator.py
+
+by Alex Courouble
+"""
+import sys, sqlite3
+DB_PATH = '/path/to/future/DB/commit_subject_short.db'
+
+# getting data from stdin and storing it in a list of tuples before populating sqlite DB
+data = sys.stdin.read()
+lines = data.split('\n')
+table = []
+for i in lines:
+	split = i.split('    ')
+	if len(split) < 4:
+		table.append(tuple(split))
+
+
+
+# creating a connection to the DB and inputing the data
+conn = sqlite3.connect(DB_PATH) # connecting
+conn.text_factory = str
+c = conn.cursor() #creating cursor
+c.execute('''create table subject(cid char(40), commit_time int, subject text)''') # creating the table
+c.executemany('INSERT INTO subject VALUES (?,?,?)', table) # importing all the data
+c.execute('CREATE INDEX subjectidx on subject(subject,commit_time)')
+c.execute('CREATE INDEX subjectidx2 on subject(cid)')
+conn.commit()
+conn.close() # we're done!
+
