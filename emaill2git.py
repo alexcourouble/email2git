c663b225c952405b3a12ffcd4079e8a401a81c8c
Alexandre Courouble <courouble.alexandre@gmail.com>
2017-05-22 10:23:06 -0400

    Small changes

--- emaill2git.py
+++ emaill2git.py
@@ -13,8 +13,8 @@ GIT_DB = '/Users/alexandrecourouble/Desktop/email2git_data/commit_subject.db'
 OUTPUT = '/Users/alexandrecourouble/Desktop/email2git_data/SUBJECT_OUTPUT.txt'
 
 # LINES
-PATCHES_PICKLED = '/Users/alexandrecourouble/Desktop/email2git_data/PATCHES_PICKLED.txt'
-COMMITS_PICKLED = '/Users/alexandrecourouble/Desktop/email2git_data/COMMITS_PICKLED.txt'
+PATCHES_PICKLED = '/Users/alexandrecourouble/Desktop/email2git_data/PATCHES_PICKLED_test.txt'
+COMMITS_PICKLED = '/Users/alexandrecourouble/Desktop/email2git_data/COMMITS_PICKLED_test.txt'
 COMMIT_MAP = '/Users/alexandrecourouble/Desktop/email2git_data/COMMIT_MAP_PICKLED.txt'
 
 MATCH_RATIO = .2
@@ -101,6 +101,7 @@ def getLineMatches():
 	readGit()
 	doAuthMapMatching()
 	doFileMapMatching()
+	doBruteMatching()
 
 
 
@@ -108,7 +109,7 @@ def readPW():
 	global PATCH_AUTHOR_MAP
 	global patches
 	with open(PATCHES_PICKLED) as f:
-		print "reading patches data"
+		print "Reading patches data"
 		patches = pickle.load(f)
 		for i in patches:
 			# CREATING FILE BASED DICT {filePath : pwid}
@@ -122,6 +123,7 @@ def readPW():
 		if auth not in PATCH_AUTHOR_MAP:
 			PATCH_AUTHOR_MAP[auth] = []
 		PATCH_AUTHOR_MAP[auth].append(i)
+	print 'Found:',len(patches),"patches."
 
 def readGit():
 	global commits
@@ -146,13 +148,14 @@ def readGit():
 
 
 def doAuthMapMatching():
+	print "Starting author map based line matches"
 	# iterate through the author map
 	countNoAuth = 0
 	for i in COMMIT_AUTHOR_MAP:
 		if COMMIT_AUTHOR_MAP[i] in PATCH_AUTHOR_MAP:
 			# print i, PATCH_AUTHOR_MAP[COMMIT_AUTHOR_MAP[i]]
 			for j in PATCH_AUTHOR_MAP[COMMIT_AUTHOR_MAP[i]]:
-				compareDiffs(i,j)
+				compareDiffs(i,j,MATCH_RATIO)
 		else:
 			print "not found: ",COMMIT_AUTHOR_MAP[i]
 			countNoAuth += 1
@@ -166,7 +169,7 @@ def doAuthMapMatching():
 
 
 def doFileMapMatching():
-
+	print "Starting file map based line matches"
 	# iterate through cid-file map
 	for i in COMMIT_FILE_MAP:
 		# if i in notMatched: # taking only commits that have NOT been matched by subject
@@ -177,13 +180,25 @@ def doFileMapMatching():
 			if j in PATCH_FILE_MAP:
 				for k in PATCH_FILE_MAP[j]:
 					if i not in MATCHED_CID and j not in MATCHED_PWID:
-						compareDiffs(i,k)
+						compareDiffs(i,k,MATCH_RATIO)
 			else:
 				print j ,"NOT FOUND"
 
 
 
-def compareDiffs(cid,pwid):
+def doBruteMatching():
+	print "Starting brute force matching on remaining of matches"
+	global MATCHED_PWID
+	global MATCHED_CID
+	for i in commits:
+		if i not in MATCHED_CID:
+			for j in patches:
+				if j not in MATCHED_PWID:
+					compareDiffs(i,j,.5)
+
+
+
+def compareDiffs(cid,pwid, threshold):
 	global MATCHED_PWID
 	global MATCHED_CID
 	doCheck = True
@@ -193,8 +208,7 @@ def compareDiffs(cid,pwid):
 		gitDiff = commits[cid]["lines"]
 	else:
 		doCheck = False
-	# patch diff
-	# print patches[pwid]["lines"]
+	# patch diff: patches[pwid]["lines"]
 	if pwid in patches:
 		pwDiff = patches[pwid]["lines"]
 	else:
@@ -202,12 +216,16 @@ def compareDiffs(cid,pwid):
 		doCheck = False
 
 	if doCheck:
+		if commits[cid]["time"] < patches[pwid]["time"]:
+			doCheck = False
+
+	if doCheck:
 		sm=difflib.SequenceMatcher(None,gitDiff,pwDiff)
 		ratio = sm.ratio()
-		if ratio > MATCH_RATIO:
+		if ratio > threshold:
 			MATCHED_CID.add(cid)
 			MATCHED_PWID.add(pwid)
-			print cid, pwid, sm.ratio()
+			# print cid, pwid, ratio
 			if cid not in lineMatches:
 				lineMatches[cid] = set([])
 			lineMatches[cid].add(pwid)
@@ -224,10 +242,17 @@ if __name__ == '__main__':
 	print "MATCHED_PWID:", len(MATCHED_PWID)
 	print "MATCHED_CID:", len(MATCHED_CID)
 
-	print "Subject commit matched: ", len(subjectMatches)
-	print "lines commit matched: ", len(lineMatches)
-	print "subject commits total: ", subjectMatchesCount
+	# print "Subject commit matched: ", len(subjectMatches)
+	# print "lines commit matched: ", len(lineMatches)
+	# print "subject commits total: ", subjectMatchesCount
 	print "len(patches)",len(patches)
 	print "len(commits)",len(commits)
 
 	print "Created in", time.time() - start
+
+
+	with open("subjectMatches.txt") as f:
+		f.write(pickle.dumps(subjectMatches))
+
+	with open("lineMatches.txt") as f:
+		f.write(pickle.dumps(lineMatches))
