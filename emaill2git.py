02e66a815c54c75448b56add34256d5d01c3dbaf
Alexandre Courouble <courouble.alexandre@gmail.com>
2017-07-27 17:38:19 -0400

    Added license info to each file

--- emaill2git.py
+++ emaill2git.py
@@ -1,5 +1,19 @@
 """
-Author: Alex Courouble <alex.courouble@gmail.com>
+Copyright (C) 2017 Alex Courouble <alex.courouble@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 import sqlite3
 import cPickle as pickle
